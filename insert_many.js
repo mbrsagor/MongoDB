7d37d8e9863690dfb006f2c4744ee2367cde96b7
Sagor <mbrsagor@gmail.com>
2020-08-16 10:27:08 +0600

    Insert many db example added

---
+++ insert_many.js
@@ -0,0 +1,42 @@
+db.user.insertMany([
+    {
+        first_name: "mbr",
+        last_name: "sagor",
+        full_name: "Md.Bozlur Rosid Sagor",
+        username: "mbrsagor",
+        email: "mbrsagor@gmail.com",
+        is_superuser: true,
+        token: "asdf3343dsfsdf232323___545453adb4",
+        password: "p12345"
+    },
+    {
+        first_name: "istiak",
+        last_name: "ohi",
+        full_name: "Md.Istiak shahorya ohi",
+        username: "ohi",
+        email: "ohi@gmail.com",
+        is_superuser: false,
+        token: "1#438f23_2323___$erer73631ere!",
+        password: "eyeyye321"
+    },
+    {
+        first_name: "al",
+        last_name: "muhij",
+        full_name: "Md.Al Muhij",
+        username: "muhuj",
+        email: "muhij@gmail.com",
+        is_superuser: false,
+        token: "orrirrif83737_!545453afgfg!ssdf",
+        password: "P0987"
+    },
+    {
+        first_name: "meg",
+        last_name: "babu",
+        full_name: "Md.Meg Babu",
+        username: "meg",
+        email: "meg.babu@gmail.com",
+        is_superuser: false,
+        token: "433433!39389382__3!!ee&3)(ori!!64ty",
+        password: "p84746363109"
+    }
+])
